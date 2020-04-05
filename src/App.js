f8a03fdbf19500090da37de944a4c71ae8045ca1
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-04-05 15:42:32 -0500

    modals working

--- src/App.js
+++ src/App.js
@@ -20,126 +20,168 @@ import UpdateGiggleLib from './components/UpdateGiggleLib';
 
 // URL to the API
 
-let baseURL = process.env.REACT_APP_BASEURL;
-
+// let baseURL = process.env.REACT_APP_BASEURL;
+let baseURL = "http://localhost:3003"
 
 
 
 class App extends React.Component {
-    // state variables
-  // template = the template with placeholder values from the database.
-  // input = user's input from GUI.
-  // giggleLib = mashup of template + input
-  
-  state = {
-    ...this.getInitialState(),
-    // Initialize user if there's a token, otherwise null
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      ...this.getInitialState(),
+      // Initialize user if there's a token, otherwise null
     user: userService.getUser(),
-  
-  
     template: "",
     input: {},
     giggleLib: "",
     giggleLibs: [],
     templates: [] 
   }
-  
 
-
-// changing this to return a JSON collection
-
-fetchGiggleLibs = new Promise((resolve, reject) => {
-  fetch(baseURL + "/gigglelibs")
-  .then((response) => response.json())
-  .then((json) => {
-    resolve(json);
-  })
-})
-
-fetchTemplates = new Promise((resolve, reject) => {
-  fetch(baseURL + "/templates")
-  .then((response) => response.json())
-  .then((json) => {
-    resolve(json);
-  })
-})
-
- // token authentication 
- getInitialState() {
-  return {
-    elapsedTime: 0,
-    isTiming: true
-  };
-}
-
-handleSignupOrLogin = () => {
-  this.setState({user: userService.getUser()});
-}
-
-handleLogout = () => {
-  userService.logout();
-  this.setState({ user: null });
-}
-
-
-
-  handleAddGiggleLib = (story) => {
-    const copyStories = [...this.state.giggleLibs]
-    copyStories.unshift(story);
-    this.setState({
-      giggleLibs: copyStories
-    })
+    this.handleItemChange = this.handleItemChange.bind(this)
   }
 
+  fetchGiggleLibs = new Promise((resolve, reject) => {
+    fetch(baseURL + "/gigglelibs")
+    .then((response) => response.json())
+    .then((json) => {
+      resolve(json);
+    })
+  })
+  
+  fetchTemplates = new Promise((resolve, reject) => {
+    fetch(baseURL + "/templates")
+    .then((response) => response.json())
+    .then((json) => {
+      resolve(json);
+    })
+  })
 
   handleNewUserInput = (newInput) => {
     this.setState({
       input: newInput
     })
   }
+
+  getInitialState() {
+    return {
+      elapsedTime: 0,
+      isTiming: true
+    };
+  }
+
+
+  handleFormUpdate() {
+    return e => {
+      const field = e.target.name
+      const { form } = this.state
+      form[field] = e.target.value
+      this.setState({ form })
+    }
+  }
+
+  handleModalHide() {
+    return () => {
+      let { giggleLibs } = this.state
+      giggleLibs = giggleLibs.map(gigglelib => ({
+        ...gigglelib,
+        showModal: false,
+      }))
+      this.setState({ giggleLibs })
+    }
+  }
+
+  handleModalShow() {
+    return e => {
+      e.preventDefault()
+
+      this.setState({ showModal: true })
+    }
+  }
+
+  handleEditItem(selectedItem) {
+    return e => {
+      e.preventDefault()
+      let { giggleLibs } = this.state
+      giggleLibs = giggleLibs.map(gigglelib => ({
+        ...gigglelib,
+        showModal: selectedItem.id === gigglelib.id,
+      }))
+      this.setState({ giggleLibs })
+    }
+  }
+
+  handleItemChange(gigglelibId) {
+    return e => {
+      let { gigglelibs } = this.state
+      gigglelibs = gigglelibs.map(gigglelib => {
+        if (gigglelib.id === gigglelibId) {
+          gigglelib[e.target.name] = e.target.value
+        }
+        return gigglelib
+      })
+      this.setState({ gigglelibs })
+    }
+  }
+
   
-  render() {
-    const { items } = this.state
-    return (
-      <div>
-            <NavBar
-        user={this.user}
-        handleLogout={this.handleLogout}
-      />
-         <Switch>
-          <Route exact path='/signup' render={({ history }) => 
-            <SignUpPage
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
-            />
-          }/>
-          <Route exact path='/login' render={({ history }) => 
-            <LoginPage
-              history={history}
-              handleSignupOrLogin={this.handleSignupOrLogin}
-            />
-          }/>
-        </Switch>
-      </div>
+  render(){
+      const { gigglelibs } = this.state
+      console.log("render-state", this.state)
+  return(
+    <div>
+     <h2>Hello World</h2>
+     {this.state.giggleLibs.map((gigglelib, index) =>
+  <div key={index} className="story">
+  
+   <a 
+    clasname="btn btn-primary"
+    onClick={this.handleEditItem(gigglelib)}
+    > <p> {gigglelib.name} </p>
+      </a>
+  </div>
+  
+)}
+{this.state.giggleLibs.map((gigglelib, index) =>
+    <ItemModal
+    key={gigglelib.id}
+    show={gigglelib.showModal}
+    onHide={this.handleModalHide()}
+    onItemChange={this.handleItemChange}
+    item={gigglelib}
+  />  
+)}
+<NewGiggleLib 
+baseURL={baseURL}
+handleAddGiggleLib={this.handleAddGiggleLib}
+makeGiggleLib={this.makeGiggleLib}
+replacer={this.replacer}
+template={this.state.templates[0]} // this appears to be an empty array not sure why since we did componentDidMount below.
+handleNewUserInput={this.handleNewUserInput} // callback fcn used to update input object in state.
+/>
+</div>
+
+  
     )
   }
-
-componentDidMount = async () => {
+  componentDidMount = async () => {
  
-      let userStories = await this.fetchGiggleLibs;
+    let userStories = await this.fetchGiggleLibs;
 
-      let allTemplates = await this.fetchTemplates;
+    let allTemplates = await this.fetchTemplates;
 
-      // checking to see if the promises have returned with the data we seek
-      // console.log("Component - got gigglelibs back: ", userStories);
-      // console.log("Component - got templates back: ", allTemplates);
+    // checking to see if the promises have returned with the data we seek
+    console.log("Component - got gigglelibs back: ", userStories);
+    console.log("Component - got templates back: ", allTemplates);
 
-      // setting these into our state
-      this.setState({
-        giggleLibs: userStories,
-        templates: allTemplates
-      })
-  }
+    // setting these into our state
+    this.setState({
+      giggleLibs: userStories,
+      templates: allTemplates
+    })
+}
 }
 
 export default App;
