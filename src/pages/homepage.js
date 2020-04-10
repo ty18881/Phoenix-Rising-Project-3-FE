ab64717f105d2f56320a33c2d3d6fc50e9fc15f8
Tara Young <ty18881@gmail.com>
2020-04-09 22:30:00 -0400

    BREAKFIX: Display EDIT form when appropriate button is pressed

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import "../css/bootstrap.css";
 
-import LoginPage from '../pages/LoginPage/LoginPage';
+
 import NewGiggleLib from '../components/NewGiggleLib';
 import UpdateGiggleLib from '../components/UpdateGiggleLib';
 
@@ -17,7 +17,7 @@ constructor(props) {
       ],
       displayForm: false,
     }
-    this.handleItemChange = this.handleItemChange.bind(this)
+    
   }
 
   handleNewUserInput = (newInput) => {
@@ -26,59 +26,7 @@ constructor(props) {
     })
   }
 
-  handleFormUpdate() {
-    return e => {
-      const field = e.target.name
-      const { form } = this.state
-      form[field] = e.target.value
-      this.setState({ form })
-    }
-  }
-
-  handleModalHide() {
-    return () => {
-      let { items } = this.state
-      items = items.map(item => ({
-        ...item,
-        showModal: false,
-      }))
-      this.setState({ items })
-    }
-  }
-
-  handleModalShow() {
-    return e => {
-      e.preventDefault()
-
-      this.setState({ showModal: true })
-    }
-  }
-
-  handleEditItem(selectedItem) {
-    return e => {
-      e.preventDefault()
-      let { items } = this.state
-      items = items.map(item => ({
-        ...item,
-        showModal: selectedItem.id === item.id,
-      }))
-      this.setState({ items })
-    }
-  }
-
-  handleItemChange(itemId) {
-    return e => {
-      let { items } = this.state
-      items = items.map(item => {
-        if (item.id === itemId) {
-          item[e.target.name] = e.target.value
-        }
-        return item
-      })
-      this.setState({ items })
-    }
-  }
-
+  
   
   handleAddGiggleLib = (story) => {
     const copyStories = [...this.props.giggleLibs]
@@ -92,41 +40,23 @@ displayUpdateForm = (story) => {
   console.log("Display Form Clicked");
   this.setState({
     displayForm: !this.state.displayForm,
-    currentStory: story
+    currentStory: story,
+    currentStoryId: story._id
   })
 
 }
 
 
   render(){
-      const {items} = this.state
-      let updateForm = "";
+      
+      let currentTemplate = {};
+      
+      if (this.state.displayForm) {
+         currentTemplate = this.props.templates.find(myTemplate => myTemplate.name === this.state.currentStory.source_template);
+      }
+      
 
-      // this method renders the UpdateGiggleLib component when the "edit" button
-      // is clicked for a story.
-      // 4/9/2020 - DEFECT - renders the update for all stories, not just the one where
-      // "edit" button is clicked.
-
-      if ( this.state.displayForm ) {
-        console.log("Displaying Form with story", this.state.currentStory);
-        // pull the template behind this story from the templates array
-
-        let currentTemplate = this.props.templates.find(myTemplate => myTemplate.name === this.state.currentStory.source_template);
-        console.log("Displaying Form with Template", currentTemplate);
-        updateForm = (
-          <div>
-            return <UpdateGiggleLib
-                   handleNewUserInput={this.handleNewUserInput}
-                   handleAddGiggleLib={this.handleAddGiggleLib}
-                   template={currentTemplate}
-                   baseURL={this.props.baseURL}
-                   giggleLib={this.state.currentStory}
-                   /> 
-          </div>
-        )
-        
-   }
-
+      
   return(
     <div>
         <div className="box">
@@ -142,18 +72,26 @@ displayUpdateForm = (story) => {
           baseURL={this.props.baseURL}
         />
          {this.props.giggleLibs.length > 0 ?
-        //  && this.templates.owner === this.props.username  
+        //  && this.props.templates.owner === this.props.username  
 
         <div>
             <h1>Your Gigglelibs</h1> 
                 <div>
-                  {this.props.giggleLibs.map((giggleLibs) => (
+                  {this.props.giggleLibs.map((giggleLibs, index) => (
                   <div key={giggleLibs.id}>
                 <h2>{giggleLibs.name}</h2>
                   <p>{giggleLibs.text}</p>
                   <button onClick={()=>this.displayUpdateForm(giggleLibs)}>Edit</button>
                   <button >Delete</button>
-                  {updateForm}
+                  
+                  {this.state.displayForm && (this.state.currentStoryId === giggleLibs._id) ? 
+                            <UpdateGiggleLib
+                                   handleNewUserInput={this.handleNewUserInput}
+                                   handleAddGiggleLib={this.handleAddGiggleLib}
+                                   template={currentTemplate}
+                                   baseURL={this.props.baseURL}
+                                   giggleLib={this.state.currentStory}
+                            />  : null}
               </div>
               
                 ))}
